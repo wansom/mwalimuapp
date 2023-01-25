3467ee782b008bbc03bab84100b46abafd656553
wansom <ochiengwarren10@gmail.com>
2023-01-25 16:56:57 +0300

    hover effect

--- src/components/forms/ProfileInformation.vue
+++ src/components/forms/ProfileInformation.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-      <!-- <div class="steps-action">
+  <div>
+    <!-- <div class="steps-action">
         <a-button v-if="current < steps.length - 1" type="primary" @click="next">
           Next
         </a-button>
@@ -15,141 +15,166 @@
           Previous
         </a-button>
       </div> -->
-      <a-row :gutter="24" type="flex" align="middle">
-        <a-col :span="4">
-            <a-card class="icon-card" @click="()=>{
-                next(1)
-            }">
-                <a-icon type="exclamation-circle" class="icon-list text-warning" />
-                <p class="card-p">General Information</p>
-                
-
-            </a-card>
-        </a-col>
-        <a-col :span="4">
-            <a-card class="icon-card" @click="()=>{
-                next(2)
-            }">
-                <a-icon type="bank" class="icon-list text-warning" />
-                <p class="card-p">Employment Information</p>
-                
-
-            </a-card>
-        </a-col>
-        <a-col :span="4">
-            <a-card class="icon-card" @click="()=>{
-                next(3)
-            }">
-                <a-icon type="folder-open" class="icon-list text-warning" />
-                <p class="card-p">Education Information</p>
-                
-
-            </a-card>
-        </a-col>
-        <a-col :span="4" @click="()=>{
-                next(4)
-            }">
-            <a-card class="icon-card">
-              
-                <a-icon type="file-protect" class="icon-list text-warning"/>
-                <p class="card-p">Documents Upload</p>
-                
-
-            </a-card>
-        </a-col>
-        <a-col :span="4">
-            <a-card class="icon-card" @click="()=>{
-                next(5)
-            }">
-                <a-icon type="transaction" class="icon-list text-warning" />
-                <p class="card-p">Payment</p>
-                
-
-            </a-card>
-        </a-col>
-
-
-      </a-row>
-<div class="my-10" v-if="current==1">
-    <GeneralInformation :user="user"/>
-</div>
-<div class="my-10" v-if="current==2">
-    <EmploymentInformation :user="user"/>
-</div>
-<div class="my-10" v-if="current==3">
-    <EducationInformation :user="user"></EducationInformation>
-</div>
-<div class="my-10" v-if="current==4">
-    <CertificateUpload :user="user"></CertificateUpload>
-</div>
-<div class="my-10" v-if="current==5">
-    <SubscriptionPayment :user="user"></SubscriptionPayment>
-</div>
+    <a-row :gutter="24" type="flex" align="middle">
+      <a-col :span="4">
+        <a-card
+          class="icon-card"
+          @click="
+            () => {
+              next(1);
+            }
+          "
+        >
+          <a-icon type="exclamation-circle" class="icon-list text-warning" />
+          <p class="card-p">General Information</p>
+        </a-card>
+      </a-col>
+      <a-col :span="4">
+        <a-card
+          class="icon-card"
+          @click="
+            () => {
+              next(2);
+            }
+          "
+        >
+          <a-icon type="bank" class="icon-list text-warning" />
+          <p class="card-p">Employment Information</p>
+        </a-card>
+      </a-col>
+      <a-col :span="4">
+        <a-card
+          class="icon-card"
+          @click="
+            () => {
+              next(3);
+            }
+          "
+        >
+          <a-icon type="folder-open" class="icon-list text-warning" />
+          <p class="card-p">Education Information</p>
+        </a-card>
+      </a-col>
+      <a-col
+        :span="4"
+        @click="
+          () => {
+            next(4);
+          }
+        "
+      >
+        <a-card class="icon-card">
+          <a-icon type="file-protect" class="icon-list text-warning" />
+          <p class="card-p">Documents Upload</p>
+        </a-card>
+      </a-col>
+      <a-col :span="4">
+        <a-card
+          class="icon-card"
+          @click="
+            () => {
+              next(5);
+            }
+          "
+        >
+          <a-icon type="transaction" class="icon-list text-warning" />
+          <p class="card-p">Payment</p>
+        </a-card>
+      </a-col>
+    </a-row>
+    <div class="my-10" v-if="current == 1">
+      <GeneralInformation :user="user" />
     </div>
-  </template>
-  <script>
-import GeneralInformation from './General-Information.vue';
-import EmploymentInformation from './Employment-Information.vue';
-import EducationInformation from './Education-Information.vue';
-import CertificateUpload from './Certificate-Upload.vue';
-import SubscriptionPayment from './Subscription-Payment.vue';
+    <div class="my-10" v-if="current == 2">
+      <EmploymentInformation :user="user" />
+    </div>
+    <div class="my-10" v-if="current == 3">
+      <EducationInformation :user="user"></EducationInformation>
+    </div>
+    <div class="my-10" v-if="current == 4">
+      <CertificateUpload :user="user"></CertificateUpload>
+    </div>
+    <div class="my-10" v-if="current == 5">
+      <SubscriptionPayment :user="user"></SubscriptionPayment>
+    </div>
+  </div>
+</template>
+<script>
+import GeneralInformation from "./General-Information.vue";
+import EmploymentInformation from "./Employment-Information.vue";
+import EducationInformation from "./Education-Information.vue";
+import CertificateUpload from "./Certificate-Upload.vue";
+import SubscriptionPayment from "./Subscription-Payment.vue";
 import * as fb from "../../firebase";
-import { mapState } from 'vuex';
-  export default {
-    data() {
-        return {
-            
-      
-        };
+import { mapState } from "vuex";
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    next(value) {
+      this.$store.dispatch("changeStep", value);
     },
-    methods: {
-        next(value) {
-            this.$store.dispatch("changeStep",value)
-        },
-        prev() {
-            this.current--;
-        },
-
+    prev() {
+      this.current--;
     },
-    components: { GeneralInformation, EmploymentInformation, EducationInformation, CertificateUpload, SubscriptionPayment },
-    computed: {
-    ...mapState(["user","current"]),
+  },
+  components: {
+    GeneralInformation,
+    EmploymentInformation,
+    EducationInformation,
+    CertificateUpload,
+    SubscriptionPayment,
+  },
+  computed: {
+    ...mapState(["user", "current"]),
   },
   mounted() {
     let user = fb.auth.currentUser;
     this.$store.dispatch("fetchUserProfile", user);
   },
 };
-  </script>
-  <style scoped>
-  .steps-content {
-    margin-top: 16px;
-    border: 1px dashed #e9e9e9;
-    border-radius: 6px;
-    background-color: #fafafa;
-    min-height: 200px;
-    text-align: center;
-    padding-top: 80px;
-  }
-  
-  .steps-action {
-    margin-top: 24px;
-  }
-  .icon-card{
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    text-align: center;
-    justify-content: center;
-    height: 200px;
-  }
-  .card-p{
-    font-size: 24;
-    font-weight: bold;
-  }
-  .icon-list{
-    font-size: 68px;
-    margin-bottom: 10px;
+</script>
+<style scoped>
+.steps-content {
+  margin-top: 16px;
+  border: 1px dashed #e9e9e9;
+  border-radius: 6px;
+  background-color: #fafafa;
+  min-height: 200px;
+  text-align: center;
+  padding-top: 80px;
+}
 
-  }
-  </style>
+.steps-action {
+  margin-top: 24px;
+}
+.icon-card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+  height: 200px;
+}
+.icon-card:hover {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+  height: 200px;
+  background-color: #db1c22;
+}
+.icon-card:hover {
+ color:#ffffff
+}
+.card-p {
+  font-size: 24;
+  font-weight: bold;
+}
+.icon-list {
+  font-size: 68px;
+  margin-bottom: 10px;
+}
+</style>
