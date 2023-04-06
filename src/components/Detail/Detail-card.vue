eb22aa8886babb5b59648cb0f5ebdb86d0156cbf
wansom <ochiengwarren10@gmail.com>
2023-04-06 16:08:16 +0300

    details

--- src/components/Detail/Detail-card.vue
+++ src/components/Detail/Detail-card.vue
@@ -94,29 +94,9 @@
       </div>
     </div>
     <div class="product-tab">
-      <div class="tab-item">
-        <ul class="nav" role="tablist">
-          <li>
-            <a class="active" data-toggle="tab" href="#tab-1" role="tab"
-              >Biography</a
-            >
-          </li>
-          <li>
-            <a data-toggle="tab" href="#tab-2" role="tab">EDUCATION BACKGROUND</a>
-          </li>
-          <li>
-            <a data-toggle="tab" href="#tab-4" role="tab">WORK EXPERIENCE</a>
-          </li>
-          <li>
-            <a data-toggle="tab" href="#tab-3" role="tab"
-              >CLIENT REVIEWS (02)</a
-            >
-          </li>
-        </ul>
-      </div>
-      <div class="tab-item-content">
-        <div class="tab-content">
-          <div class="tab-pane fade-in active" id="tab-1" role="tabpanel">
+      <a-tabs default-active-key="1" @change="callback">
+      <a-tab-pane key="1" tab="BIOGRAPHY">
+        <div class="tab-pane fade-in active" id="tab-1" role="tabpanel">
             <div class="product-content">
               <div class="row">
                 <div class="col-lg-7">
@@ -132,22 +112,11 @@
               </div>
             </div>
           </div>
-          <div class="tab-pane fade" id="tab-2" role="tabpanel">
-            <div class="specification-table">
+      </a-tab-pane>
+     
+      <a-tab-pane key="2" tab="EDUCATION BACKGROUND" force-render>
+        <div class="specification-table">
               <table>
-                <!-- <tr>
-                  <td class="p-catagory">Client Rating</td>
-                  <td>
-                    <div class="pd-rating">
-                      <i class="fa fa-star"></i>
-                      <i class="fa fa-star"></i>
-                      <i class="fa fa-star"></i>
-                      <i class="fa fa-star"></i>
-                      <i class="fa fa-star-o"></i>
-                      <span>(5)</span>
-                    </div>
-                  </td>
-                </tr> -->
                 <tr>
                   <td class="p-catagory">LAW SCHOOL</td>
                   <td>
@@ -168,23 +137,10 @@
                 </tr>
               </table>
             </div>
-          </div>
-          <div class="tab-pane fade" id="tab-4" role="tabpanel">
-            <div class="specification-table">
+      </a-tab-pane>
+      <a-tab-pane key="3" tab="WORK EXPERIENCE">
+        <div class="specification-table">
               <table>
-                <!-- <tr>
-                  <td class="p-catagory">Client Rating</td>
-                  <td>
-                    <div class="pd-rating">
-                      <i class="fa fa-star"></i>
-                      <i class="fa fa-star"></i>
-                      <i class="fa fa-star"></i>
-                      <i class="fa fa-star"></i>
-                      <i class="fa fa-star-o"></i>
-                      <span>(5)</span>
-                    </div>
-                  </td>
-                </tr> -->
                 <tr>
                   <td class="p-catagory">{{ advocate.current_employer }}</td>
                   <td>
@@ -216,78 +172,10 @@
                
               </table>
             </div>
-          </div>
-          <div class="tab-pane fade" id="tab-3" role="tabpanel">
-            <div class="customer-review-option">
-              <h4>2 Comments</h4>
-              <div class="comment-option">
-                <div class="co-item">
-                  <div class="avatar-pic">
-                    <img src="https://acelitigator.com/wp-content/uploads/2023/01/MicrosoftTeams-image-7.png" alt="" />
-                  </div>
-                  <div class="avatar-text">
-                    <div class="at-rating">
-                      <i class="fa fa-star"></i>
-                      <i class="fa fa-star"></i>
-                      <i class="fa fa-star"></i>
-                      <i class="fa fa-star"></i>
-                      <i class="fa fa-star-o"></i>
-                    </div>
-                    <h5>Brandon Kelley <span>27 Aug 2019</span></h5>
-                    <div class="at-reply">Nice !</div>
-                  </div>
-                </div>
-                <div class="co-item">
-                  <div class="avatar-pic">
-                    <img src="https://acelitigator.com/wp-content/uploads/2023/01/MicrosoftTeams-image-7.png" alt="" />
-                  </div>
-                  <div class="avatar-text">
-                    <div class="at-rating">
-                      <i class="fa fa-star"></i>
-                      <i class="fa fa-star"></i>
-                      <i class="fa fa-star"></i>
-                      <i class="fa fa-star"></i>
-                      <i class="fa fa-star-o"></i>
-                    </div>
-                    <h5>Roy Banks <span>27 Aug 2019</span></h5>
-                    <div class="at-reply">Nice !</div>
-                  </div>
-                </div>
-              </div>
-              <div class="personal-rating">
-                <h6>Your Ratind</h6>
-                <div class="rating">
-                  <i class="fa fa-star"></i>
-                  <i class="fa fa-star"></i>
-                  <i class="fa fa-star"></i>
-                  <i class="fa fa-star"></i>
-                  <i class="fa fa-star-o"></i>
-                </div>
-              </div>
-              <div class="leave-comment">
-                <h4>Leave A Comment</h4>
-                <form action="#" class="comment-form">
-                  <div class="row">
-                    <div class="col-lg-6">
-                      <input type="text" placeholder="Name" />
-                    </div>
-                    <div class="col-lg-6">
-                      <input type="text" placeholder="Email" />
-                    </div>
-                    <div class="col-lg-12">
-                      <textarea placeholder="Messages"></textarea>
-                      <button type="submit" class="site-btn">
-                        Send comment
-                      </button>
-                    </div>
-                  </div>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      </a-tab-pane>
+    </a-tabs>
     </div>
+    
   </div>
 </template>
 
