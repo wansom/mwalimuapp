9c947b2c69d699bcdf3a25ccada2ea79bf386609
wansom <ochiengwarren10@gmail.com>
2023-04-11 12:10:18 +0300

    fix:dial a lawyer mobile

--- src/components/Cards/CardInfo.vue
+++ src/components/Cards/CardInfo.vue
@@ -1,5 +1,6 @@
 <template>
-  <a-card :bordered="false" class="card-info" style="max-width: 920px; width: 40vw;  min-height: 334px; margin-right:20px; border-radius: 10px;">
+  <section>
+    <a-card :bordered="false" class="card-info d-none d-md-block" >
     <a-row type="flex">
       <a-col class="col-img d-block d-md-none" :span="24" :md="12">
         <div>
@@ -69,6 +70,79 @@
       </a-col>
     </a-row>
   </a-card>
+  <div class="small-card block d-md-none">
+    <a-row class="align-items-center mb-2">
+      <a-col class="col-img " :span="12">
+        <div>
+          <a-avatar :src="advocate.profile_photo" :size="124" style="object-fit:contain" loading="lazy"/>
+        </div>
+      </a-col>
+      <a-col :span="12">
+        <h5 class="mb-2">{{ advocate.first_name }} {{ advocate.last_name }}</h5>
+          <h6>{{ advocate.job_title }}</h6>
+      </a-col>
+    </a-row>
+    <a-row type="flex">
+      
+      <a-col class="col-content" :span="24" :xl="12">
+        <div class="card-content">
+         
+          <a-row type="flex" class="my-5">
+            <a-col :span="12">
+              <div>
+                <a-icon type="environment" class="mx-5" />{{
+                  advocate.location
+                }}
+              </div>
+              <div>
+                <a-icon type="calendar" class="mx-5" />{{
+                  new Date().getFullYear() -
+                  new Date(advocate.practise_start).getFullYear()
+                }}Years Experience
+              </div>
+            </a-col>
+            <a-col :span="12">
+              <div>
+                <a-icon type="bank" class="mx-5" />{{
+                  advocate.current_employer
+                }}
+              </div>
+              <div>
+                <a-icon type="clock-circle" class="mx-5" />Member since
+                {{ advocate.subscription_date }}
+              </div>
+            </a-col>
+          </a-row>
+          <p>
+            {{ advocate.biography.slice(0,150) }}
+          </p>
+            <h6>Practise Areas</h6>
+          <div style="display: flex; flex-wrap: wrap">
+            <div class="my-5 mx-5">
+              <a-button><a-icon type="up-circle" style="color:#DB1C22;" />{{ advocate.specialisation}}</a-button>
+            </div>
+            <div
+              class="my-5 mx-5"
+              v-for="i of advocate.practise_areas"
+              :key="i"
+            >
+              <a-button>{{ i }}</a-button>
+            </div>
+          </div>
+        </div>
+        <div>
+          <router-link :to="'/advocate/'+advocate.id">
+           <a-button type="primary" block>
+            View Profile
+       
+            </a-button>
+          </router-link>
+        </div>
+      </a-col>
+    </a-row>
+  </div>
+  </section>
+
 </template>
 
 <script>
@@ -99,5 +173,11 @@ export default {
   margin-top: 20px;
   font-size: 24px;
   color: #db1c22;
+}
+.small-card{
+  width: 100%;
+  height: fit-content;
+  padding: 5px 15px;
+  background: #ffffff;
 }
 </style>
