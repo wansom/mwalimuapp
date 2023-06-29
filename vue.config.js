7023e498a3787002a1721a0a80fa49a847e56967
wansom <ochiengwarren10@gmail.com>
2023-06-29 10:22:25 +0300

    sitemap

--- vue.config.js
+++ vue.config.js
@@ -9,5 +9,16 @@ module.exports = {
 				
 				return args
 			})
-	}
+	},
+	pluginOptions: {
+		sitemap: {
+		  urls: [
+			'https://dialalawyer.africa/',
+			'https://dialalawyer.africa/find-a-lawyer',
+			'https://dialalawyer.africa/contact-us',
+			'https://dialalawyer.africa/sign-up',
+			'https://dialalawyer.africa/sign-in'
+		  ]
+		}
+	  }
 }
